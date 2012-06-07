d18bc52a81d2cc9a06fddacbc9385cd820eb68bc
Esteban Pellegrino <pellegre.esteban@gmail.com>
2012-06-07 11:21:07 -0300

    Add the Types.h file to the commit

---
+++ libcrafter/crafter/Types.h
@@ -0,0 +1,21 @@
+/*
+ * Types.h
+ *
+ *  Created on: Jun 7, 2012
+ *      Author: larry
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <stdint.h>
+
+namespace Crafter {
+
+	typedef uint32_t word;
+	typedef uint16_t short_word;
+	typedef uint8_t byte;
+
+}
+
+#endif /* TYPES_H_ */
