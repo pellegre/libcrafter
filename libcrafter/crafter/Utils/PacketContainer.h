4605610913f2e7abc153aae082e1dcc29c3226cd
Esteban Pellegrino <pellegre.esteban@gmail.com>
2012-06-14 23:14:11 -0300

    Developing a Send function with generic foward iterators

--- libcrafter/crafter/Utils/PacketContainer.h
+++ libcrafter/crafter/Utils/PacketContainer.h
@@ -10,6 +10,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <iterator>
 #include <vector>
 #include <string>
 #include <pcap.h>
@@ -34,6 +35,22 @@ namespace Crafter {
 		s << t;
 		return s.str();
 	}
+
+	template<typename FowardIter>
+	struct ThreadData {
+		/* Container interators */
+		FowardIter beginIterator;
+		FowardIter endIterator;
+		FowardIter outputIterator;
+		/* Arguments for sending */
+		std::string iface;
+		int num_threads;
+		double timeout;
+		int retry;
+	};
+
+	/* Function called by a thread to send some packets */
+	void* SendThread(void* thread_arg);
 
 	class PacketContainer : public std::vector<Packet*> {
 
@@ -94,7 +111,87 @@ namespace Crafter {
 		}
 	}
 
-	/* Send the packets */
+	/* +++++++++++++++ Send the packets +++++++++++++++++++++ */
+
+	/* This function is executed by a thread */
+	template<typename FowardIter>
+	void* SendThread(void* thread_arg) {
+
+		/* Cast the argument */
+		ThreadData<FowardIter>* pair = static_cast<ThreadData<FowardIter> *>(thread_arg);
+
+		/* Assign the values */
+		FowardIter begin = pair->beginIterator;
+		FowardIter end = pair->endIterator;
+
+		ApplyPacketFunction(begin,end,&Packet::Send,pair->iface);
+
+		for (int i = start ; i < total ; i += num_threads)
+			(*PktContainer)[i]->Send(pair->iface);
+
+		delete pair;
+
+		/* Call pthread exit */
+		pthread_exit(NULL);
+	}
+
+	template<typename FowardIter>
+	void SendMultiThread(FowardIter begin, FowardIter end, const std::string& iface, int num_threads) {
+		/* Total number of packets */
+		size_t total = distance(begin,end);
+		if (total < num_threads) num_threads = total;
+
+		/* Thread array */
+		pthread_t* threads = new pthread_t[num_threads];
+
+		/* Do the work on each packet */
+		for(int i = 0 ; i < num_threads ; i++) {
+			/* Create a pair structure */
+			ThreadData<FowardIter>* pair = new ThreadData<FowardIter>;
+
+			/* Start value on the container */
+			pair->beginIterator = advance(begin,i);
+			/* Put the number of threads */
+			pair->num_threads = num_threads;
+			/* Put the size of the container */
+			pair->endIterator = end;
+
+			/* Set the arguments for the SendRecv function */
+			pair->iface = iface;
+
+			void* thread_arg = static_cast<void *>(pair);
+
+			int rc = pthread_create(&threads[i], NULL, SendThread, thread_arg);
+
+			if (rc) {
+				PrintMessage(Crafter::PrintCodes::PrintError,
+						     "Crafter::Send()",
+				             "Creating thread. Returning code = " + toString(rc));
+				exit(1);
+			}
+
+		}
+
+		/* Join thread */
+		for(int i = 0 ; i < num_threads ; i++) {
+			void* ret;
+
+			/* Join thread */
+			int rc = pthread_join(threads[i], &ret);
+
+			if (rc) {
+				PrintMessage(Crafter::PrintCodes::PrintError,
+						     "BlockARP()",
+				             "Joining thread. Returning code = " + toString(rc));
+				exit(1);
+			}
+
+		}
+
+		delete [] threads;
+
+	}
+
 	template<typename FowardIter>
 	void Send(FowardIter begin, FowardIter end, const std::string& iface = "", int num_threads = 0) {
 		ApplyPacketFunction(begin,end,&Packet::Send,iface);
