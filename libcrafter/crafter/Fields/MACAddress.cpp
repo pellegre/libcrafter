fbefdec904ceab196fb24777642754277615efc1
Gregory Detal <gregory.detal@uclouvain.be>
2013-06-20 10:51:46 +0200

    MAC OSX portage
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- libcrafter/crafter/Fields/MACAddress.cpp
+++ libcrafter/crafter/Fields/MACAddress.cpp
@@ -24,13 +24,76 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
+#ifndef __APPLE__
 #include <netinet/ether.h>
+#else
+#include <netinet/if_ether.h>
+#endif
 #include <cstdio>
 #include "MACAddress.h"
 
 using namespace std;
 using namespace Crafter;
+
+#if __APPLE__
+#ifndef isdigit
+#define isdigit(c)  (c >= '0' && c <= '9')
+#endif
+#ifndef islower
+#define islower(c)  (c >=  'a' && c <= 'z')
+#endif
+#ifndef isspace
+#define isspace(c)  (c ==  ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
+#endif
+#ifndef isupper
+#define isupper(c)  (c >=  'A' && c <= 'Z')
+#endif
+#ifndef tolower
+#define tolower(c)  (isupper(c) ? ( c - 'A' + 'a') : (c))
+#endif
+#ifndef toupper
+#define toupper(c)  (islower(c) ? (c - 'a' + 'A') : (c))
+#endif
+
+/*
+  ether_aton code from glibc, GPL'd.
+ */
+struct ether_addr *ether_aton_r(const char *asc, struct ether_addr *addr)
+{
+	size_t cnt;
+
+	for (cnt = 0; cnt < 6; ++cnt) {
+		unsigned int number;
+		char ch;
+
+		ch = tolower (*asc);
+		asc++;
+		if ((ch < '0' || ch > '9') && (ch < 'a' || ch > 'f'))
+			return NULL;
+		number = isdigit (ch) ? (ch - '0') : (ch - 'a' + 10);
+
+		ch = tolower (*asc);
+		if ((cnt < 5 && ch != ':') || (cnt == 5 && ch != '\0' && !isspace (ch))) {
+			++asc;
+			if ((ch < '0' || ch > '9') && (ch < 'a' || ch > 'f'))
+				return NULL;
+			number <<= 4;
+			number += isdigit (ch) ? (ch - '0') : (ch - 'a' + 10);
+
+			ch = *asc;
+			if (cnt < 5 && ch != ':')
+				return NULL;
+		}
+
+		/* Store result.  */
+		addr->ether_addr_octet[cnt] = (unsigned char) number;
+
+		/* Skip ':'.  */
+		++asc;
+	}
+	return addr;
+}
+#endif
 
 MACAddress::MACAddress(const std::string& name, size_t nword, size_t nbyte) :
 					 Field<std::string> (name,nword,nbyte*8,48),
