8de4c43e4e7d77b55f01d80915f0f044eab08c52
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-05-22 20:24:09 -0300

    feat(net): add network utility helpers

--- crates/crafter/src/lib.rs
+++ crates/crafter/src/lib.rs
@@ -7,11 +7,16 @@ pub use crafter_core::*;
 pub use crafter_live as live;
 pub use crafter_net as net;
 pub use crafter_net::{
-    reply_filter, reply_matches, send_packet, send_packets, send_plan, send_recv_packet,
-    send_recv_packets, BatchSend, BatchSendEntry, BatchSendRecv, BatchSendRecvEntry,
-    BatchSendRecvReport, BatchSendReport, NetError, PacketBatchSendExt, PacketBatchSendRecvExt,
-    PacketSendExt, PacketSendRecvExt, RawSender, ReplyMatcher, SendMode, SendOptions, SendPlan,
-    SendRecv, SendRecvOptions, SendRecvReport, SendReport, SendTarget, SocketSend, SocketSender,
+    arp_resolve, default_interface, default_interface_in, default_interface_name,
+    derive_mac_from_ipv6, find_interface, find_interface_in, get_ip_strings, get_ips, get_mac,
+    get_my_ip, get_my_ip_in, get_my_ipv6, get_my_ipv6_in, get_my_mac, get_my_mac_in, interface_for,
+    interface_for_in, interfaces, parse_ip_range, parse_numbers, reply_filter, reply_matches,
+    resolve_mac, send_packet, send_packets, send_plan, send_recv_packet, send_recv_packets,
+    ArpResolveOptions, ArpResolveReport, BatchSend, BatchSendEntry, BatchSendRecv,
+    BatchSendRecvEntry, BatchSendRecvReport, BatchSendReport, InterfaceAddress, InterfaceInfo,
+    Ipv4Range, NetError, PacketBatchSendExt, PacketBatchSendRecvExt, PacketSendExt,
+    PacketSendRecvExt, RawSender, ReplyMatcher, SendMode, SendOptions, SendPlan, SendRecv,
+    SendRecvOptions, SendRecvReport, SendReport, SendTarget, SocketSend, SocketSender,
 };
 pub use crafter_pcap as pcap;
 pub use crafter_pcap::{
@@ -24,9 +29,14 @@ pub use crafter_pcap::{
 pub mod prelude {
     pub use crafter_core::*;
     pub use crafter_net::{
-        reply_filter, reply_matches, send_packet, send_packets, send_plan, send_recv_packet,
-        send_recv_packets, BatchSend, BatchSendEntry, BatchSendRecv, BatchSendRecvEntry,
-        BatchSendRecvReport, BatchSendReport, NetError, PacketBatchSendExt, PacketBatchSendRecvExt,
+        arp_resolve, default_interface, default_interface_in, default_interface_name,
+        derive_mac_from_ipv6, find_interface, find_interface_in, get_ip_strings, get_ips, get_mac,
+        get_my_ip, get_my_ip_in, get_my_ipv6, get_my_ipv6_in, get_my_mac, get_my_mac_in,
+        interface_for, interface_for_in, interfaces, parse_ip_range, parse_numbers, reply_filter,
+        reply_matches, resolve_mac, send_packet, send_packets, send_plan, send_recv_packet,
+        send_recv_packets, ArpResolveOptions, ArpResolveReport, BatchSend, BatchSendEntry,
+        BatchSendRecv, BatchSendRecvEntry, BatchSendRecvReport, BatchSendReport, InterfaceAddress,
+        InterfaceInfo, Ipv4Range, NetError, PacketBatchSendExt, PacketBatchSendRecvExt,
         PacketSendExt, PacketSendRecvExt, RawSender, ReplyMatcher, SendMode, SendOptions, SendPlan,
         SendRecv, SendRecvOptions, SendRecvReport, SendReport, SendTarget, SocketSend,
         SocketSender,
