76c088eeb0b616aaf0b293db8bad28cb3c33b52a
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-21 12:15:20 -0300

    refactor(whad): move vendored protos into the backend dir

--- crafter/build.rs
+++ crafter/build.rs
@@ -11,17 +11,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[cfg(feature = "whad")]
 fn generate_whad_proto() -> Result<(), Box<dyn std::error::Error>> {
     let protos = [
-        "whad-proto/generic.proto",
-        "whad-proto/device.proto",
-        "whad-proto/ble.proto",
-        "whad-proto/whad.proto",
+        "src/wire/backend/whad/proto/generic.proto",
+        "src/wire/backend/whad/proto/device.proto",
+        "src/wire/backend/whad/proto/ble.proto",
+        "src/wire/backend/whad/proto/whad.proto",
     ];
-    let includes = ["whad-proto"];
+    let includes = ["src/wire/backend/whad/proto"];
 
-    println!("cargo:rerun-if-changed=whad-proto/generic.proto");
-    println!("cargo:rerun-if-changed=whad-proto/device.proto");
-    println!("cargo:rerun-if-changed=whad-proto/ble.proto");
-    println!("cargo:rerun-if-changed=whad-proto/whad.proto");
+    println!("cargo:rerun-if-changed=src/wire/backend/whad/proto/generic.proto");
+    println!("cargo:rerun-if-changed=src/wire/backend/whad/proto/device.proto");
+    println!("cargo:rerun-if-changed=src/wire/backend/whad/proto/ble.proto");
+    println!("cargo:rerun-if-changed=src/wire/backend/whad/proto/whad.proto");
 
     let mut config = prost_build::Config::new();
     config.include_file("whad_proto.rs");
