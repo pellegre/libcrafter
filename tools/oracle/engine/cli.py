3ccb00347aebaca0692f91c60559fc26e00a10f9
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-21 03:55:02 -0300

    test(ble): add oracle radio spec

--- tools/oracle/engine/cli.py
+++ tools/oracle/engine/cli.py
@@ -8503,6 +8503,7 @@ def _self_check(args: argparse.Namespace) -> int:
 
 
 _SUITE_FEATURE_BY_FAMILY = {
+    "ble": "ble-radio-phdr",
     "dns": "dns_behavior",
     "igmp": "igmp_header",
     "ip": "ip_fragment_transforms",
