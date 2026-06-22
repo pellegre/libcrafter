c8f2a67b542a30b68ac3a0f41e14fae853977d4b
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-22 00:13:38 -0300

    feat(dot15d4): add dot15d4 link decode entrypoint

--- crafter/src/protocols/link/dot15d4/mod.rs
+++ crafter/src/protocols/link/dot15d4/mod.rs
@@ -13,6 +13,12 @@ mod nwk;
 mod radio;
 
 pub use aps::ZigbeeAps;
+pub use consts::{Dot15d4AddrMode, Dot15d4FrameType};
 pub use mac::Dot15d4;
 pub use nwk::ZigbeeNwk;
 pub use radio::Dot15d4Radio;
+
+pub(crate) use aps::decode_zigbee_aps;
+pub(crate) use mac::decode_dot15d4;
+pub(crate) use nwk::decode_zigbee_nwk;
+pub(crate) use radio::decode_dot15d4_radio;
