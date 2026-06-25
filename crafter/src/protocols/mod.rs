94a5f468546a4cfe5c50aa1d31ab4e5499f72d5a
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-25 02:52:24 -0300

    feat(snmp): add usm engine time helpers

--- crafter/src/protocols/mod.rs
+++ crafter/src/protocols/mod.rs
@@ -454,7 +454,7 @@ pub mod exports {
         snmp_v3_flags_label, Snmp, SnmpErrorStatus, SnmpErrorStatusAssignment, SnmpErrorStatusMeta,
         SnmpGetBulkPdu, SnmpOid, SnmpPdu, SnmpPduTagMeta, SnmpPduTagStatus, SnmpRawPdu,
         SnmpRawPduBody, SnmpRawSecurityParameters, SnmpRequestPdu, SnmpScopedPdu,
-        SnmpSecurityModel, SnmpSecurityModelMeta, SnmpSecurityModelStatus,
+        SnmpSecurityModel, SnmpSecurityModelMeta, SnmpSecurityModelStatus, SnmpUsmEngineTime,
         SnmpUsmSecurityParameters, SnmpV1TrapPdu, SnmpV3Flags, SnmpV3GlobalData, SnmpV3Message,
         SnmpVarBind, SnmpVarBindList, SnmpVersion, SNMP_ERROR_STATUS_AUTHORIZATION_ERROR,
         SNMP_ERROR_STATUS_BAD_VALUE, SNMP_ERROR_STATUS_COMMIT_FAILED, SNMP_ERROR_STATUS_GEN_ERR,
