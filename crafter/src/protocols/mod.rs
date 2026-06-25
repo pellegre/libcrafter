603664318a14c035fb0d72c22913bd79507bb500
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-25 20:50:57 -0300

    feat(quic): build retry packets

--- crafter/src/protocols/mod.rs
+++ crafter/src/protocols/mod.rs
@@ -421,8 +421,8 @@ pub mod exports {
     pub use ospf::Ospf;
     pub use quic::constants::{QUIC_VERSION_1, QUIC_VERSION_2, QUIC_VERSION_NEGOTIATION};
     pub use quic::{
-        Quic, QuicConnectionId, QuicFrame, QuicPacket, QuicPacketNumber, QuicRetryPacket,
-        QuicTransportParameter, QuicVarInt, QuicVersionNegotiationBuilder,
+        Quic, QuicConnectionId, QuicFrame, QuicPacket, QuicPacketNumber, QuicRetryBuilder,
+        QuicRetryPacket, QuicTransportParameter, QuicVarInt, QuicVersionNegotiationBuilder,
         QuicVersionNegotiationPacket, QUIC_RETRY_INTEGRITY_TAG_LEN,
     };
     pub use rip::ripng::{
