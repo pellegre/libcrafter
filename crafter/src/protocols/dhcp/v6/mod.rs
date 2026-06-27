801238968128cde54fd0a8d4e4c34fb045510ba8
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-26 22:34:47 -0300

    feat(dhcpv6): add ia na option

--- crafter/src/protocols/dhcp/v6/mod.rs
+++ crafter/src/protocols/dhcp/v6/mod.rs
@@ -21,7 +21,9 @@ pub use duid::Dhcpv6Duid;
 pub(crate) use layer::{append_dhcpv6_packet, looks_like_dhcpv6_payload};
 pub use layer::{Dhcpv6, Dhcpv6RelayHeader};
 pub use message::Dhcpv6MessageType;
-pub use option::{Dhcpv6Option, Dhcpv6OptionCode, Dhcpv6OptionFormat, Dhcpv6OptionValue};
+pub use option::{
+    Dhcpv6IaNa, Dhcpv6Option, Dhcpv6OptionCode, Dhcpv6OptionFormat, Dhcpv6OptionValue,
+};
 pub use registry::{
     dhcpv6_option_meta, dhcpv6_option_name, dhcpv6_option_status, Dhcpv6ClientOro,
     Dhcpv6OptionMeta, Dhcpv6OptionSingleton, Dhcpv6OptionStatus,
