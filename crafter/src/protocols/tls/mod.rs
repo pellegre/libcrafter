d734b6b0ea58dcfd1b05ec0ada80b9fbd0a46f9d
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-06-30 21:58:45 -0300

    feat(tls): add record header

--- crafter/src/protocols/tls/mod.rs
+++ crafter/src/protocols/tls/mod.rs
@@ -25,6 +25,10 @@ pub use extension::{
     TLS_EXTENSION_TYPE_LEN,
 };
 pub use named_group::{TlsNamedGroup, TlsNamedGroupList};
+pub use record::{
+    TlsRecordHeader, TLS_RECORD_CONTENT_TYPE_LEN, TLS_RECORD_HEADER_LEN, TLS_RECORD_LENGTH_LEN,
+    TLS_RECORD_VERSION_LEN,
+};
 pub use signature_scheme::{TlsSignatureScheme, TlsSignatureSchemeList};
 pub use version::{TlsVersion, TlsVersionField};
 
