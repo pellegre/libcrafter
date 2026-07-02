7dc7b28dba53ccfcd4b266dda2a2b921eeef2dc9
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-07-01 21:26:20 -0300

    feat(ntp): add fixed point helpers

--- crafter/src/protocols/ntp/mod.rs
+++ crafter/src/protocols/ntp/mod.rs
@@ -10,19 +10,21 @@
 pub mod constants;
 pub mod message;
 pub mod registry;
+pub mod time;
 
 pub use self::constants::*;
 pub use self::message::{
     append_ntp_packet, decode_ntp_payload, looks_like_ntp_payload, ntp_client_udp,
     ntp_ipv4_client_request, ntp_ipv6_client_request, ntp_pack_first_octet, ntp_parse_first_octet,
     ntp_server_udp, Ntp, NtpExtensionField, NtpLeapIndicator, NtpLegacyMac, NtpMode,
-    NtpReferenceId, NtpShortFormat, NtpStratum, NtpTimestamp, NtpVersion,
+    NtpReferenceId, NtpStratum, NtpTimestamp, NtpVersion,
 };
 pub use self::registry::{
     ntp_extension_field_type_meta, ntp_kiss_o_death_code_meta, ntp_leap_indicator_meta,
     ntp_mode_meta, ntp_nts_extension_field_type_meta, ntp_reference_id_meta, ntp_stratum_meta,
     NtpReferenceCodeMeta, NtpRegistryMeta, NtpRegistryStatus,
 };
+pub use self::time::NtpShortFormat;
 
 #[cfg(test)]
 mod tests {
