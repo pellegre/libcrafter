cd7c1928d3d952a5557af9016d83ab3f56876d92
Esteban Pellegrino <pellegre.esteban@gmail.com>
2026-07-05 16:37:21 -0300

    feat(flow): add run options

--- tools/flow/src/lib.rs
+++ tools/flow/src/lib.rs
@@ -4,10 +4,12 @@ pub mod bound;
 pub mod binding;
 pub mod docaddr;
 pub mod error;
+pub mod options;
 pub mod prelude;
 pub mod role;
 
 pub use bound::Bound;
 pub use binding::{BindMode, BindSendClass, BindTarget, Binding};
 pub use error::{FlowError, Result};
+pub use options::RunOptions;
 pub use role::Role;
